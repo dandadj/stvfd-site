825bddfba5e1a85d601377479a95cb1a479bce62
Danny Brady <dannyjbrady@gmail.com>
2015-08-30 22:46:50 -0400

    Changed backend db to disk-db for node. Now can get all, single, or add incidents through RESTful api.

--- server.js
+++ server.js
@@ -5,9 +5,10 @@
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
-var JsonDB = require('node-json-db');
+var db = require('diskdb');
 
-var db = new JsonDB("myDataBase", true, true);
+db = db.connect('./db', ['incidents']);
+console.log("There are currently " + db.incidents.count() + " incidents in the DB")
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -40,25 +41,46 @@ router.route('/incidents')
 
 // create an incident (accessed at POST http://localhost:8080/api/incidents)
 .post(function (req, res) {
+        var new_id = 1;
+        try{
+            var all_incidents = db.incidents.find();
+            if(all_incidents.length > 0) {
+                var max_id = all_incidents.map(function(inc) {
+                                        return(parseInt(inc.id));
+                                    });
+                new_id = Math.max.apply(Math, max_id) + 1;
+            }
+        }
+        catch(error) {
+            console.error(error);
+        }
+
         var title = req.body.title; // set the bears name (comes from the request)
         var description = req.body.description;
+        var date = req.body.date;
+        var units = req.body.units;
 
-        db.push("/incidents", {
+        db.incidents.save({
+            id: new_id,
             title: title,
-            description: description
+            description: description,
+            date: date,
+            units: units
         });
         res.json({
-            message: "Incident added"
+            message: "Incident added with id " + new_id,
+            id: new_id
         })
     })
     .get(function (req, res) {
-        var incidents = db.getData("/incidents");
+        var incidents = db.incidents.find();
         res.json(incidents);
     });
 
 router.route('/incidents/:incident_id')
     .get(function (req, res) {
-        res.json({message: "Need to implement"});
+        var incident = db.incidents.findOne({"id":req.params.incident_id});
+        res.json(incident);
     });
 
 // more routes for our API will happen here
